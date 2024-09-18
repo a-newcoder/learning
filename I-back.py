7e49a867a0807b63a33fb2c5d0768142b7df667b
WeiZe-Lu <854316318@qq.com>
2024-09-18 08:16:07 +0800

    2024-09-01

--- I-back.py
+++ I-back.py
@@ -1,22 +1,7 @@
 print("This is my 2024.2.8 go back to python and protect it again ")
 print("实现了一个二分查找")
 
-if __name__ == "__main__":
-    new = [1,2,3,4,5]
-    right = (len(new) - 1)
-    left = 0
-    target = int(input("输入目标"))
+print("hallo world")
+if 1:
+    print("1")
 
-    while True:
-        mid = left + (right - left) // 2
-        text = new[mid]
-        if left > right:
-            print("-1")
-            break
-        if text == target:
-            print(mid)
-            break
-        elif target < text:
-            right = mid - 1
-        else:
-            left = mid + 1
